d7b27c140e3785614957c74eedc927d2653821c8
BlazeEMP <blazelitanui@gmail.com>
2025-02-25 19:33:22 -0600

    new code

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -7,6 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
+    console.log('This is new code.');
     await mongoose.connect(MONGODB_URI);
     console.log('Database connected.');
     return mongoose.connection;
